3d53a106975a6b3a6073e9937d85221e0dee9841
IVmxrina <marinacuemag@gmail.com>
2026-08-09 23:52:28 +0200

    Adicion de migraciones

---
+++ NinaPOS/Migrations/20260809215133_InicialConUsuarios.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NinaPOS.Migrations
+{
+    /// <inheritdoc />
+    public partial class InicialConUsuarios : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DNI = table.Column<string>(type: "TEXT", nullable: false),
+                    Telefono = table.Column<string>(type: "TEXT", nullable: false),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Residencia = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CodigoBarras = table.Column<string>(type: "TEXT", nullable: false),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Precio = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Categoria = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transacciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Total = table.Column<decimal>(type: "TEXT", nullable: false),
+                    MetodoPago = table.Column<string>(type: "TEXT", nullable: false),
+                    UsuarioId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transacciones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CodigoEmpleado = table.Column<string>(type: "TEXT", nullable: false),
+                    Contrasena = table.Column<string>(type: "TEXT", nullable: false),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Rol = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clientes_DNI",
+                table: "Clientes",
+                column: "DNI",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Productos_CodigoBarras",
+                table: "Productos",
+                column: "CodigoBarras",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "Transacciones");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
--- NinaPOS/Models/NinaPosDbContext.cs
+++ NinaPOS/Models/NinaPosDbContext.cs
@@ -7,15 +7,23 @@ namespace NinaPOS.Models
 {
     public class NinaPosDbContext : DbContext
     {
+        public NinaPosDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
         public static string DbPath =>
         Path.Combine(FileSystem.AppDataDirectory, "ninapos.db");
 
         public DbSet<Producto> Productos => Set<Producto>();
         public DbSet<Cliente> Clientes => Set<Cliente>();
         public DbSet<Transaccion> Transacciones => Set<Transaccion>();
+        public DbSet<Usuario> Usuarios => Set<Usuario>();
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite($"Data Source={DbPath}");
+        {
+            if (!options.IsConfigured)
+                options.UseSqlite($"Data Source={DbPath}");
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
