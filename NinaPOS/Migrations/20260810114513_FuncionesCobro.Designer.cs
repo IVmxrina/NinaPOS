5ad55992d9502b6e6e716454c604787cf3874b70
IVmxrina <marinacuemag@gmail.com>
2026-08-11 10:30:06 +0200

    Realizacion de migraciones

--- NinaPOS/Migrations/20260810114513_FuncionesCobro.Designer.cs
+++ NinaPOS/Migrations/20260810114513_FuncionesCobro.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NinaPOS.Models;
 
@@ -10,9 +11,11 @@ using NinaPOS.Models;
 namespace NinaPOS.Migrations
 {
     [DbContext(typeof(NinaPosDbContext))]
-    partial class NinaPosDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260810114513_FuncionesCobro")]
+    partial class FuncionesCobro
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.10");
